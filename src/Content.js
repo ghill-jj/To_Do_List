ede5be2de9d76e0c9a1ea373ac9d195b9474486d
Ghill JJ <ghilljj@gmail.com>
2023-09-29 12:40:41 +0530

    intialized basic buttons and functionality

--- src/Content.js
+++ src/Content.js
@@ -1,17 +1,49 @@
 import React from "react";
 import { useState } from "react";
+import { RiDeleteBin2Line } from "react-icons/ri";
 
 const Content = () => {
-  const [name, setName] = useState("?");
-  function handleValueChange() {
-    const values = ["Fame", "Power", "Money", "Love"];
-    const change = Math.floor(Math.random() * 4);
-    setName(values[change]);
-  }
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      checked: true,
+      item: "Practice Coding",
+    },
+    {
+      id: 2,
+      checked: false,
+      item: "Learn React",
+    },
+    {
+      id: 3,
+      checked: true,
+      item: "Learn Sql",
+    },
+  ]);
+
+  const handleCheck = (id) => {
+    const listItems = items.map((item) =>
+      item.id === id ? { checked: !item.checked } : item
+    );
+    setItems(listItems);
+  };
+
   return (
     <main>
-      <p>We Live for {name}</p>
-      <button onClick={handleValueChange}>Submit</button>
+      <ul>
+        {items.map((item) => (
+          <li className="item" key={item.id}>
+            []
+            <input
+              type="checkbox"
+              onChange={() => handleCheck(item.id)}
+              checked={item.checked}
+            />
+            <label>{item.item}</label>
+            <RiDeleteBin2Line role="button" tabIndex={0} />
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
