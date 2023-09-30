d6ac3008db2ba2382c3a125c3eae419f503200bb
Ghill JJ <ghilljj@gmail.com>
2023-09-30 18:08:54 +0530

    added new input column
    -created a new AddItem component for adding new items to the old items
    -created a new useState for uploading new items by input element

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Content from "./Content";
 import { useState } from "react";
 import Footer from "./Footer";
+import AddItem from "./AddItem";
 
 const App = () => {
   const [items, setItems] = useState([
@@ -23,6 +24,15 @@ const App = () => {
     },
   ]);
 
+  const [newItem, setNewItem] = useState("");
+
+  const addItem = (item) => {
+    const id = items.length ? items[items.length - 1].id + 1 : 1;
+    const addNewItem = { id, checked: false, item };
+    const listItems = [...items, addNewItem];
+    setItems(listItems);
+    localStorage.setItem("to_do_list", JSON.stringify(listItems));
+  };
   const handleCheck = (id) => {
     const listItems = items.map((item) =>
       item.id === id ? { ...item, checked: !item.checked } : item
@@ -31,14 +41,27 @@ const App = () => {
     localStorage.setItem("to_do_list", JSON.stringify(listItems));
   };
   const handleDelete = (id) => {
-    const removeItem = items.filter((item) => item.id !== id);
-    setItems(removeItem);
-    localStorage.setItem("to_do_lists", JSON.stringify(removeItem));
+    const listItems = items.filter((item) => item.id !== id);
+    setItems(listItems);
+    localStorage.setItem("to_do_lists", JSON.stringify(listItems));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!newItem) return;
+    console.log(newItem);
+    addItem(newItem);
+    setNewItem("");
   };
 
   return (
     <div className="App">
       <Header title="To_Do_List" />
+      <AddItem
+        newItem={newItem}
+        setNewItem={setNewItem}
+        handleSubmit={handleSubmit}
+      />
       <Content
         items={items}
         handleCheck={handleCheck}
